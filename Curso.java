f1582f256cb8953f9edb4ff14700052de7fec2ef
Erick Barrera <dannbj1@gmail.com>
2023-09-21 08:17:50 -0600

    creacion clase Curso

---
+++ Curso.java
@@ -0,0 +1,65 @@
+/**
+ * Universidad del Valle de Guatemala
+ * Departamento de Ciencias de la Computación
+ * Programación Orientada a Objetos - CC2008 - 50
+ * 
+ * @author: Erick Barrera
+ * @description: clase que almacena los datos de los cursos
+ * @version: 1.0
+ * @created: 21/09/23
+ * @last_modified: 21/09/23
+ */
+public class Curso {
+    // atributos
+    private Profesor profesor;
+    private String nombre, codigo, horario;
+    private int cantPeriodos, cantEstudiantes;
+
+    // constructor
+    public Curso(String nombre, String codigo, String horario, int cantPeriodos, int cantEstudiantes, Profesor profesor){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.horario = horario;
+        this.cantPeriodos = cantPeriodos;
+        this.cantEstudiantes = cantEstudiantes;
+        this.profesor = profesor;
+    }
+    // getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getHorario(){return horario;}
+    public void setHorario(String horario){this.horario = horario;}
+
+    public int getCantPeriodos() {
+        return cantPeriodos;
+    }
+    public void setCantPeriodos(int cantPeriodos) {
+        this.cantPeriodos = cantPeriodos;
+    }
+    
+    public int getCantEstudiantes() {
+        return cantEstudiantes;
+    }
+    public void setCantEstudiantes(int cantEstudiantes) {
+        this.cantEstudiantes = cantEstudiantes;
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+}
